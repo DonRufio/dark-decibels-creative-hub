d3ffdfb6d4480aa31c32375eadd63185b767db23
Raffaele Puzzuoli <raffaelepuzzuoli@gmail.com>
2024-11-21 01:57:29 +0000

    tetst

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename={isProduction ? "/dark-decibels-creative-hub/" : "/"}>
+        <BrowserRouter basename={isProduction ? "/" : "/"}>
           <Routes>
             <Route path="/" element={<Index />} />
           </Routes>
